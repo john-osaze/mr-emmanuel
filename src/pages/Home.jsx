fddb074064c85b5a415a943bbd2aee331da8401c
John Osaze <johnfavourosaze@gmail.com>
2022-06-18 12:41:41 +0100

    Initial Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+	<div>
+		<Footer />
+		<Navbar />
+	</div>
+  )
+}
+
+export default Home
